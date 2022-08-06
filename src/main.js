cf2595ee2bea06f0f1c29d2548fd800684078fbf
Ghalip <ghalip603@gmail.com>
2022-08-06 18:33:49 +0800

    修改  .gitignore

--- src/main.js
+++ src/main.js
@@ -1,11 +1,26 @@
-    const http = require('http');
+const http = require('http');
 
-    const server = http.createServer((request, response) => {
-        response.write('Hello~');
-        response.end();
+const server = http.createServer((request, response) => {
+
+    const data = {
+        id: 1,
+        title: '阿力甫学习',
+        content: '阿力甫的开发之路之Json格式的数据'
+
+    }
+
+    const jsonData = JSON.stringify(data);
+
+    response.writeHead(200,{
+        'Content-Type': 'application/json; charset=utf-8'
     });
 
+    response.write(jsonData);
 
-    server.listen(3000 , () => {
-        console.log('🚀服务已启动！');
-    });
+    response.end();
+});
+
+
+server.listen(3000, () => {
+    console.log('🚀服务已启动！');
+});
